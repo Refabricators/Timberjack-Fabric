7dd6135d3482412b1b140873c4f79c7a4a7636ea
TechPro424 <smsvra6@gmail.com>
2023-05-30 12:49:50 +0530

    Make the mod id a separate field for convinient access

--- src/main/java/org/refabricators/timberjack/Timberjack.java
+++ src/main/java/org/refabricators/timberjack/Timberjack.java
@@ -9,7 +9,8 @@ public class Timberjack implements ModInitializer {
     // This logger is used to write text to the console and the log file.
     // It is considered best practice to use your mod id as the logger's name.
     // That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("timberjack-refabricated");
+    public static final String MOD_ID = "timberjack-refabricated";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
