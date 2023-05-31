b6329be1d91de1b7f81c2c2b5dbe623830367b2f
TechPro424 <smsvra6@gmail.com>
2023-05-31 19:23:09 +0530

    use Path instead of File wherever possible

--- src/main/java/org/refabricators/timberjack/config/JsonOperations.java
+++ src/main/java/org/refabricators/timberjack/config/JsonOperations.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 
 import com.google.gson.GsonBuilder;
 
@@ -13,16 +14,15 @@ import com.google.gson.Gson;
 
 public class JsonOperations {
     public static Config config;
-    private final static File settingsFile = new File(FabricLoader.getInstance().getConfigDir().toString() + "/timberjack.json");
+    private final static String configFilePath = FabricLoader.getInstance().getConfigDir().toString() + "/timberjack.json";
     private final static Gson gson = new GsonBuilder().registerTypeAdapter(Config.class, new ConfigTypeAdapter()).setPrettyPrinting().create();
 
     public static Config loadConfigFromFile() {
         try {
-            config = gson.fromJson(new String(Files.readAllBytes(settingsFile.toPath()), StandardCharsets.UTF_8),
+            config = gson.fromJson(configFilePath,
                 Config.class);
         } catch (Exception ex) {
             System.out.println("Error while loading config! Creating a new one!");
-            ex.printStackTrace();
             config = new Config();
             writeConfig();
         }
@@ -31,9 +31,10 @@ public class JsonOperations {
     }
 
     public static void writeConfig() {
+        File settingsFile = new File(configFilePath);
         if (settingsFile.exists()) settingsFile.delete();
         try {
-            Files.write(settingsFile.toPath(), gson.toJson(config).getBytes(StandardCharsets.UTF_8));
+            Files.write(Path.of(configFilePath), gson.toJson(config).getBytes(StandardCharsets.UTF_8));
         } catch (IOException e1) {
             e1.printStackTrace();
         }
